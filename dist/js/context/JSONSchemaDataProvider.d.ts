562677d7b7d94a0ef66e43ea378716d308f46b15
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2025-12-01 23:17:31 +0200

    chore: remove JSONSchemaDataProvider and JinjaContextProvider dist

--- dist/js/context/JSONSchemaDataProvider.d.ts
+++
@@ -1,9 +0,0 @@
-import type { JSONSchema } from "@mat3ra/esse/dist/js/esse/utils";
-import JinjaContextProvider from "./JinjaContextProvider";
-/**
- * @summary Provides jsonSchema only.
- */
-declare abstract class JSONSchemaDataProvider extends JinjaContextProvider {
-    get jsonSchema(): JSONSchema;
-}
-export default JSONSchemaDataProvider;
